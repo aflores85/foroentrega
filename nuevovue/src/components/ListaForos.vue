e45649c017970e1c07a0e6d35734b243023885b9
aflores85 <aflores85@gmail.com>
2022-05-31 19:48:22 -0300

    first commit

---
+++ nuevovue/src/components/ListaForos.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>LISTADO DE FOROS ACTIVOS</h1>
+  <ul v-for="(foro, index) in foros" :key="index">
+    <li>{{ foro }}</li>
+  </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListaForos",
+  data() {
+    return {
+      foros: [],
+    };
+  },
+  methods: {
+    getForos() {
+      fetch("http://localhost:5000/api/v1/getforo")
+        .then((response) => response.json())
+        .then((data) => {
+          this.foros = data.foros;
+        });
+    },
+  },
+  mounted() {
+    this.getForos();
+  },
+};
+</script>
+
+<style scoped></style>
