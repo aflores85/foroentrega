c76cf7c716f538359d91fdd74b10fd1b9e23c287
aflores85 <aflores85@gmail.com>
2022-06-05 18:08:41 -0300

    master

--- nuevovue/src/views/InicioView.vue
+++ nuevovue/src/views/InicioView.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div>
-  
+ 
   <header style="text-align:center;">
   <hola-mundo-comp></hola-mundo-comp>
   </header>
@@ -11,8 +11,10 @@
   <Form-Subject></Form-Subject>
   <h4 style="background-color:darkolivegreen"> <lista-foros></lista-foros></h4>
   <Lista-Subject style="background-color:darkgrey"></Lista-Subject>
+    <Contacto></Contacto>  
   </div>
   
+
 
 </template>
 
@@ -23,12 +25,14 @@ import ListaForos from "../components/ListaForos";
 import FormForo from "../components/FormForo";
 import FormSubject from "../components/FormSubject";
 import ListaSubject from "../components/ListaSubject";
+import Contacto from "../components/Contacto";
+
 
 
 
 export default {
   name: "InicioView",
-  components: { HolaMundoComp, ListaForos, FormForo, FormSubject, ListaSubject },
+  components: { HolaMundoComp, ListaForos, FormForo, FormSubject, ListaSubject, Contacto },
     
 };
 </script>
